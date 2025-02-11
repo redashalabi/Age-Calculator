445f9476551a220a16e294f198e5eaee1a4e3bc5
SoleSprint <redavd12@gmail.com>
2025-02-11 14:02:11 +0200

    Update calculateAge.js
    Signed-off-by: SoleSprint <redavd12@gmail.com>

--- calculateAge.js
+++ calculateAge.js
@@ -1,19 +1,23 @@
-function calculateAge() {
-    const dob = document.getElementById('dob').value;
-    if (dob === "") {
-        document.getElementById('result').innerHTML = "Please select your date of birth!";
-        return;
-    }
-
-    const birthDate = new Date(dob);
-    const today = new Date();
-
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const monthDifference = today.getMonth() - birthDate.getMonth();
-    
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthDate.getDate())) {
-        age--;
-    }
-
-    document.getElementById('result').innerHTML = `You are ${age} years old.`;
-}
+ function calculateAge() {
+            let dob = document.getElementById('dob').value;
+            if (dob === "") {
+                document.getElementById('result').innerText = "يرجى إدخال تاريخ الميلاد";
+                return;
+            }
+            let dobDate = new Date(dob);
+            let today = new Date();
+            let ageYears = today.getFullYear() - dobDate.getFullYear();
+            let ageMonths = today.getMonth() - dobDate.getMonth();
+            let ageDays = today.getDate() - dobDate.getDate();
+            
+            if (ageDays < 0) {
+                ageMonths--;
+                ageDays += new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+            }
+            if (ageMonths < 0) {
+                ageYears--;
+                ageMonths += 12;
+            }
+            
+            document.getElementById('result').innerText = `عمرك هو ${ageYears} سنة و ${ageMonths} شهر و ${ageDays} يوم`;
+        }
